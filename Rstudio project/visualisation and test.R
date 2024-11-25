8253d341c20e3a651c569aa4a2b222db4c45699c
ruhullah12 <rh24abf@herts.ac.uk>
2024-11-25 00:54:16 +0000

    correlation test and scatter plot

--- Rstudio project/visualisation and test.R
+++ Rstudio project/visualisation and test.R
@@ -3,9 +3,17 @@ WHP<-read_csv("World_happiness_report_2019.csv")
 ds<-WHP
 names (ds)[3] <- "score"
 names (ds)[4] <- "GDP"
+#histogram to show normalcy of data 
 h <- hist(ds$score, main = "Histogram of Happiness Score")
+# Add a normal distribution line
 x <- seq(min(ds$score),max(ds$score),length=100)
 y <- dnorm (x, mean=mean(ds$score),sd= sd(ds$score))* length(ds$score)
 box.size<- diff(h$mids[1:2])
 y<- y * box.size
 lines(x,y, col="red")
+#correlation 
+cor.test(ds$score, ds$GDP)  
+cor.test(ds$score, ds$GDP, method="pearson")  
+plot(ds$GDP, ds$score, xlab = "GDP Per Capita", ylab = "Happiness Score", main = "Scatterplot of GDP per Capita vs Happiness Score")
+# Add a trend line  
+abline(lm(score ~ GDP, data = ds), col = "red") 
