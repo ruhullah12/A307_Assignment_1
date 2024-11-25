d95fa5b37be6693ffb00800dab5a6fa1e1870bc9
ehteshamulhaq1122 <eh23abm@herts.ac.uk>
2024-11-25 00:41:27 -0800

    added normal distribution line to histogram

--- Rstudio project/visualisation and test.R
+++ Rstudio project/visualisation and test.R
@@ -3,4 +3,9 @@ WHP<-read_csv("World_happiness_report_2019.csv")
 ds<-WHP
 names (ds)[3] <- "score"
 names (ds)[4] <- "GDP"
-hist(ds$score, main = "Histogram of Happiness Score")
+h <- hist(ds$score, main = "Histogram of Happiness Score")
+x <- seq(min(ds$score),max(ds$score),length=100)
+y <- dnorm (x, mean=mean(ds$score),sd= sd(ds$score))* length(ds$score)
+box.size<- diff(h$mids[1:2])
+y<- y * box.size
+lines(x,y, col="red")
